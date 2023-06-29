5adf699523440b1653d36d12cc62a65c0a35cc9d
Spencer Ke <kespencer88@gmail.com>
2023-06-29 12:39:20 -0700

    Finished data scrape for system startup

--- src/utils/eventScrape.ts
+++ src/utils/eventScrape.ts
@@ -3,6 +3,12 @@ import { Event } from '../entities/Event';
 import { activityRepo } from '../repositories/Activity';
 import { Activity } from '../entities/Activity';
 import * as cheerio from 'cheerio';
+import { eventRepo } from '../repositories/Event';
+import { arch } from 'os';
+import { ActivityModel } from '../models/Activity';
+import { Tournament } from '../entities/Tournament';
+import { tournamentRepo } from '../repositories/Tournament';
+import * as fs from 'fs';
 
 function fetchPage(url: string): Promise<string|undefined> {
     const HTMLData =
@@ -21,29 +27,89 @@ function extractEventData(doc: string): any{
     const $event_lists = $('.em-events-list');
     const $month_headers = $event_lists.find('h2');
     const events: Array<Event> = [];
-    $month_headers.each((index, $elem) => {
+    $month_headers.each((_index, $elem) => {
         // Element order is:
         //           title, button,      null,       table
         let $table = $elem.nextSibling.nextSibling.nextSibling;
         let $tbody = $($table).find('tbody');
         let $row = $($tbody).find('tr');
-        $row.each((index, $row_elem ) => {
+        $row.each((_index, $row_elem ) => {
             if ($($row_elem).hasClass('cancelled')){return};
 
             let date_array: Array<string> = $($elem).text().split(' ');
 
+            //scrape 3 years in advance
             if(parseInt(date_array[date_array.length-1]) >= (new Date().getFullYear()) + 3){return};
 
-            date_array.splice(1, 0, $($row_elem).find('.event-time').html().replace(/\s+/g, '').split('<br>')[0]);
             var row_event = new Event();
 
-            row_event.date = date_array.join(' ');
-            
+            var days = $($row_elem).find('.event-time').html().replace(/\s+/g, '').split('<br>')[0].split('-');
+            var startDate = new Date(date_array[0] + ' ' + days[0] + ', ' + date_array[1]);
+            var endDate = new Date(date_array[0] + ' ' + days[1] + ', ' + date_array[1]);
+            row_event.startDate = startDate;
+            row_event.endDate = endDate;
+
             row_event.name = $($row_elem).find('.event-description').text();
+
             let location_data: Array<string> = $($row_elem).find('.event-category-notes').text().split('(');
             row_event.host = location_data[0];
             row_event.location = location_data[1] ? location_data[1].split(')')[0] : '';
-            console.log(row_event);
+            
+            eventRepo.createAndSave(row_event).then(
+                (create_result) => {
+
+                    //add activities
+
+                    //Shenanigans are present at every event
+                    let shenanigans: Promise<Array<ActivityModel>> =activityRepo.findByName('Shenanigans');
+                    shenanigans.then((activity_array) => {eventRepo.addActivity(create_result, [activity_array[0]]);});
+
+                    //add declared activities
+                    var $activity_div = $($row_elem).find('.event-activities');
+                    var $activity_logos = $($activity_div).find('i');
+                    $activity_logos.each((_index, $activity_elem) => {
+                        let logo_title_array = {
+                            'Has Court': 'Court',
+                            'has heavy fighting': 'Heavy',
+                            'has youth activities': 'Youth Combat',
+                            'has rapier and/or cut and thrust fighting': 'Rapier/C&T',
+                            'has archery': 'Archery',
+                            'has thrown weapons': 'Thrown Weapons',
+                            'has bardic activites': 'Bardic'
+                        };
+                        if(logo_title_array.hasOwnProperty($activity_elem.attribs.title)){
+                            let event_activity: Promise<Array<ActivityModel>> = activityRepo.findByName(logo_title_array[$activity_elem.attribs.title]);
+                            event_activity.then((activity_array) => {
+                                eventRepo.addActivity(create_result, [activity_array[0]]);
+
+                                //add tournament for every activity but Court and Shenanigans
+                                if(logo_title_array[$activity_elem.attribs.title] != 'Court'){
+                                    let activity_tournament = new Tournament();
+                                    activity_tournament.event = create_result;
+                                    activity_tournament.name = row_event.name + ' ' + logo_title_array[$activity_elem.attribs.title] + ' Tournament';
+                                    activity_tournament.type = activity_array[0];
+                                    tournamentRepo.createAndSave(activity_tournament);
+                                }
+                            });
+                        }
+                    });
+
+                    //get specific location
+                    fetchPage($($row_elem).find('.event-description').attr('href')).then(
+                        ($event_page) => {
+                            let site_info = $($event_page).find('h3.event-heading').next().next().children().first().text().split('\n');
+                            if(site_info.length == 5){
+                                if(site_info[3] == 'Online'){
+                                    row_event.location = site_info[3];
+                                } else {
+                                    row_event.location = `${site_info[site_info.length - 2].trim()} ${site_info[site_info.length - 1].trim()}`;
+                                }
+                                eventRepo.save(row_event);
+                            }
+                        }
+                    );
+                }
+            );            
         });
     });
     return null;
@@ -57,6 +123,7 @@ function addActivities(): void {
         'Archery',
         'Thrown Weapons',
         'Bardic',
+        'Shenanigans',
         'Court'
     ];
     
