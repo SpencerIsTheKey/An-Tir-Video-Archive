c5010f4103d0409172d2da7cdd469f233ef3376a
Spencer Ke <kespencer88@gmail.com>
2023-08-10 14:40:00 -0700

    create auth middleware for use in post endpoints

---
+++ backend/src/api/middleware/authMiddleware.ts
@@ -0,0 +1,24 @@
+import * as jwt from 'jsonwebtoken';
+import { Request, Response, NextFunction } from 'express';
+
+export const SECRET_KEY: jwt.Secret = process.env.JWT_SECRET;
+
+export interface CustomRequest extends Request{
+    token: string | jwt.JwtPayload;
+}
+
+export const auth = (req: Request, res: Response, next: NextFunction) => {
+    try{
+        const token = req.header('Authorization')?.replace('Bearer ', '');
+
+        if (!token) {
+            throw new Error();
+        }
+        const decoded = jwt.verify(token, SECRET_KEY);
+        (req as CustomRequest).token = decoded;
+
+        next();
+    } catch (err) {
+        res.status(401).send('401: Unauthorized. Please authenticate to authorize access');
+    }
+};
